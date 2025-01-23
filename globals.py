482d158b15cdc4f91369d593c266b225f7dc1c5f
pdxlocations <benlipsey@gmail.com>
2025-01-23 12:29:14 -0800

    sort globals

--- globals.py
+++ globals.py
@@ -1,25 +1,27 @@
 import os
 
+# App Variables
 app_directory = os.path.dirname(os.path.abspath(__file__))
-db_file_path = os.path.join(app_directory, "client.db")
-log_file_path = os.path.join(app_directory, "client.log")
-
+interface = None
+display_log = False
 all_messages = {}
 channel_list = []
 notifications = set()
 packet_buffer = []
+node_list = []
 myNodeNum = 0
 selected_channel = 0
 selected_message = 0
 selected_node = 0
 current_window = 0
-interface = None
-display_log = False
+
+# User Configurable
+db_file_path = os.path.join(app_directory, "client.db")
+log_file_path = os.path.join(app_directory, "client.log")
 message_prefix = ">>"
 sent_message_prefix = message_prefix + " Sent"
+notification_symbol = "*"
 ack_implicit_str = "[◌]"
 ack_str = "[✓]"
 nak_str = "[x]"
 ack_unknown_str = "[…]"
-notification_symbol = "*"
-node_list = []
